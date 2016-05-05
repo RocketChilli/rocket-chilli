839c8c67fa4726d3cf8f5ec7d9c9645a0e2c4b4d
Jonathan Holvey <jonathan.holvey@outlook.com>
2016-05-05 19:10:31 +1000

    Prepare index.php with links to external resources
    Blank JS and CSS files
    Header and footer templates
    CSS fonts
    Google tracking code
    jQuery 2.2.3
    normalize.css
    Icons and theme colour
    
    Also site verification meta tag

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>Rocket Chilli</title>
+	<meta charset="UTF-8"/>
+	<meta name="description" content=""/>
+	<meta name="keywords" content=""/>
+	<meta name="google-site-verification" content="tpup9wO6weLiHt7Y3ZaJ98X-CeX4ZoxB0xnwDIJj0Vc"/>
+	<?php include("templates/resources.php") ?>
+</head>
+<body>
+	<?php include("templates/tracking.php") ?>
+	<?php include("templates/header.php") ?>
+	<?php include("templates/footer.php") ?>
+</body>
+</html>
---
+++ templates/resources.php
@@ -0,0 +1,9 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="images/icon.ico"/>
+<link rel="icon" type="image/png" href="images/icon.png" sizes="192x192"/>
+<link rel="stylesheet" type="text/css" href="styles/normalize.css"/>
+<link rel="stylesheet" type="text/css" href="styles/fonts.css"/>
+<link rel="stylesheet" type="text/css" href="styles/common.css"/>
+<script src="script/jquery-2.2.3.min.js"></script>
+<script type="text/javascript" src="script/common.js"></script>
+<meta name="theme-color" content="#384447"/>
