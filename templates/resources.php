09a3c56b83b1c1af454cebc37dc36e4c4d10d9e9
Jonathan Holvey <jonathan.holvey@outlook.com>
2016-05-07 16:44:05 +1000

    Update and minify normalize.css and move to libraries folder

--- templates/resources.php
+++ templates/resources.php
@@ -1,7 +1,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="shortcut icon" href="images/icon.ico"/>
 <link rel="icon" type="image/png" href="images/icon.png" sizes="192x192"/>
-<link rel="stylesheet" type="text/css" href="styles/normalize.css"/>
+<link rel="stylesheet" type="text/css" href="libraries/normalize-4.1.1.min.css"/>
 <link rel="stylesheet" type="text/css" href="styles/fonts.css"/>
 <link rel="stylesheet" type="text/css" href="styles/common.scss"/>
 <script src="libraries/jquery-2.2.3.min.js"></script>
