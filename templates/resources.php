88deaf11db71bdf2f14a26d632d5a8464a81a1e8
Jonathan Holvey <jonathan.holvey@outlook.com>
2016-05-10 19:25:53 +1000

    Add 16 and 48px resolution PNG icons

--- templates/resources.php
+++ templates/resources.php
@@ -1,6 +1,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/png" href="images/icon-16.png" sizes="16x16"/>
+<link rel="icon" type="image/png" href="images/icon-48.png" sizes="48x48"/>
+<link rel="icon" type="image/png" href="images/icon-192.png" sizes="192x192"/>
 <link rel="shortcut icon" href="images/icon.ico"/>
-<link rel="icon" type="image/png" href="images/icon.png" sizes="192x192"/>
 <link rel="stylesheet" type="text/css" href="libraries/normalize-4.1.1.min.css"/>
 <link rel="stylesheet" type="text/css" href="styles/fonts.css"/>
 <link rel="stylesheet" type="text/css" href="styles/common.scss"/>
