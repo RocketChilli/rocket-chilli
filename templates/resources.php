760dea00473c8a91ca19c18933cd88697549d337
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-08-07 20:42:35 +1000

    Tweak page styles and rename resource files

--- templates/resources.php
+++ templates/resources.php
@@ -3,6 +3,6 @@
 <link rel="icon" type="image/png" href="images/icon-48.png" sizes="48x48"/>
 <link rel="icon" type="image/png" href="images/icon-192.png" sizes="192x192"/>
 <link rel="shortcut icon" href="images/icon.ico"/>
-<link rel="stylesheet" type="text/css" href="styles/common.scss"/>
-<script type="text/javascript" src="script/common.js"></script>
+<link rel="stylesheet" type="text/css" href="styles/main.scss"/>
+<script type="text/javascript" src="script/main.js"></script>
 <meta name="theme-color" content="#384447"/>
