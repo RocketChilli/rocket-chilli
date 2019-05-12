d907a80a67e5ff744bf5a8c2104986f3738aded1
Jonathan Holvey <jonathan.holvey@outlook.com>
2019-05-12 11:19:21 +1000

    Move container build directory to /opt/app

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,13 @@ FROM httpd:2.4-alpine
 
 RUN apk add --update nodejs-npm
 
-WORKDIR /usr/local/apache2/htdocs
-COPY package*.json ./
-RUN npm install --prod && npm run build
+WORKDIR /opt/app
+RUN mkdir ./public && \
+    rm -rf /usr/local/apache2/htdocs && \
+    ln -sf /opt/app/public /usr/local/apache2/htdocs
 
-COPY * ./
+COPY web/package*.json ./
+RUN npm install --prod
+
+COPY web/ ../app/
+RUN npm run build
--- web/gulpfile.js
+++ web/gulpfile.js
@@ -5,22 +5,28 @@ const sass = require('gulp-sass')
 const tildeImporter = require('node-sass-tilde-importer')
 const livereload = require('gulp-livereload')
 
-const sourcePath = './src'
-const scssSource = `${sourcePath}/scss/main.scss`
-const publicPath = `./public`
+const source = {
+  scss: './src/scss',
+  public: './public',
+}
+
+const dest = {
+  root: './public',
+  css: './public/css',
+}
 
 // CSS injection on SCSS compile
 gulp.task('sass-dev', () => (
-  gulp.src(scssSource)
+  gulp.src(`${source.scss}/main.scss`)
     .pipe(sass({ importer: tildeImporter }))
     .on('error', console.log)
-    .pipe(gulp.dest(publicPath))
+    .pipe(gulp.dest(dest.css))
     .pipe(livereload())
 ))
 
-// Full page reload on HTML changes
+// Full page reload on static file changes
 gulp.task('reload', (done) => {
-  gulp.src(sourcePath)
+  gulp.src(source.public)
     .pipe(livereload())
   done()
 })
@@ -28,16 +34,16 @@ gulp.task('reload', (done) => {
 // Local dev watcher
 gulp.task('watch', () => {
   livereload.listen()
-  gulp.watch('scss/**/*.scss', { cwd: `${sourcePath}/` }, gulp.series('sass-dev'))
-  gulp.watch('*.html', { cwd: '.' }, gulp.series('reload'))
+  gulp.watch('**/*.scss', { cwd: source.scss }, gulp.series('sass-dev'))
+  gulp.watch(['**/*', '!css/*'], { cwd: source.public }, gulp.series('reload'))
 })
 
 // Build SCSS for production
-gulp.task('sass-prod', () => (
-  gulp.src(scssSource)
+gulp.task('sass', () => (
+  gulp.src(source.scss)
     .pipe(sass({importer: tildeImporter})) 
-    .pipe(gulp.dest(publicPath))
+    .pipe(gulp.dest(dest.css))
 ))
 
 gulp.task('default', gulp.series('sass-dev', 'watch'))
-gulp.task('prod', gulp.series('sass-prod'))
+gulp.task('prod', gulp.series('sass'))
